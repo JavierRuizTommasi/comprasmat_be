216f2dc5ae3f84db1978d075d917f27d996f6279
Javier Ruiz Tommasi <63374706+JavierRuizTommasi@users.noreply.github.com>
2020-06-18 07:27:07 -0300

    update port

--- config/properties.js
+++ config/properties.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: process.env.PORT || 8080,
+  PORT: process.env.PORT || 5006,
   // DB: 'mongodb://localhost:27017/loginJWT',
   DB: 'mongodb://myTest:myTest@66.97.44.230:27017/test',
   // PUBLIC: 'public/cliente/dist/cliente'
