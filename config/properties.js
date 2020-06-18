c18f0d1e055a01cb344d2c2b6a05fa7143dfe10a
Javier Ruiz Tommasi <63374706+JavierRuizTommasi@users.noreply.github.com>
2020-06-18 07:15:17 -0300

    connect to Mongodb

--- config/properties.js
+++ config/properties.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   // DB: 'mongodb://localhost:27017/loginJWT',
   DB: 'mongodb://myTest:myTest@66.97.44.230:27017/test',
-  PUBLIC: 'public/cliente/dist/cliente'
-  // PUBLIC: 'http://proveedores.ml'
+  // PUBLIC: 'public/cliente/dist/cliente'
+  PUBLIC: 'http://proveedores.ml'
 }
